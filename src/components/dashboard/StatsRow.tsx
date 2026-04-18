3b107f0d328b3da39084bce7ec2d5afef85dc403
Alawiye Sodeeq <alawiyesodeq@gmail.com>
2026-04-18 17:43:45 +0100

    added changes to mobile vwersion of dashbpard

--- src/components/dashboard/StatsRow.tsx
+++ src/components/dashboard/StatsRow.tsx
@@ -69,7 +69,7 @@ export const StatsRow = ({ role, data, loading }: Props) => {
   }
 
   return (
-    <div className="grid grid-cols-2 xl:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 xl:grid-cols-4 gap-4">
       {statMap[role]}
     </div>
   )
